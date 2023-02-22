b2be42ea72a40fb6d517571a88dd14bbc46d4566
rooocket <info@rooocket.ru>
2023-02-22 04:11:54 +0300

    Добавлен favicon

--- index.php
+++ index.php
@@ -97,16 +97,14 @@ $message = 'Hello';
 $tg = $api_xdrive->sendMessageTelegram($message);
 var_dump($tg['response'][0]['ok']);
 */
-?>
-
-<!doctype html>
+?><!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-	<title>ApiXDRIVE simple form</title>
+	<title>Welcome to Faberlic</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="shortcut icon" href="img/favicon/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
